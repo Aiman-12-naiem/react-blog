9a6355a17629043b16a74f320f874493087b0554
Aiman-12-naiem <78222766+Aiman-12-naiem@users.noreply.github.com>
2021-02-03 23:18:26 +0500

    Assignment $ lint error fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 // importing files from components
 import Navbar from './components/Navbar.js';
+import Home from './components/home.js';
+import Sidebar from './components/sidebar.js';
+import Footer from './components/footer.js';
+
+
 // App function
 function App() {
   return (
     <div className="App">
       {/* print Navbar */}
       <Navbar />
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-8">
+          <Home />
+          </div>
+          <div className="col-4">
+          <Sidebar />
+          </div>
+        </div>
+      </div>
+     
+   
+      <Footer />
+      
     </div>
   );
 }
---
+++ src/components/home.js
@@ -0,0 +1,29 @@
+const home = () => {
+    return (
+     
+        <div className="card mt-5">
+          <div className="card-header bg-dark">
+            <h3 className="text-light">Welcome to React!</h3>
+          </div>
+          <div className="card-body">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+              voluptas esse vel minus cupiditate earum culpa quam inventore
+              accusantium rem impedit cumque quos et, tenetur eum quaerat
+              obcaecati consequuntur tempora.
+            </p>
+          </div>
+          <div className="card-footer">
+            <p className="float-left">
+              Reposted <b>12</b>
+            </p>
+            <p className="float-right">
+              Replies <b>16</b>
+            </p>
+          </div>
+        </div>
+ 
+      );
+}
+
+export default home;
